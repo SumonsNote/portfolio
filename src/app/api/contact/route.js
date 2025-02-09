d4e7865a359badf445bcaed6e13fe094a765f704
sumon <sohankhance@gmail.com>
2025-02-10 04:09:29 +0600

    update smtp

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: false, // true for 465, false for other ports
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
